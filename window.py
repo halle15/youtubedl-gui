f03424ee4cbe1ae8861e41d29d97241e78e6f60d
tjensen <49765787+tjensen43@users.noreply.github.com>
2022-10-12 13:30:11 -0400

    Added functional video download and better main(?)

--- window.py
+++ window.py
@@ -1,26 +1,42 @@
 from __future__ import unicode_literals
-from cmath import log
+from optparse import Values
 import youtube_dl
 
 import PySimpleGUI as sg
 
-#rest of the code
+
+
+""" BEGIN CODE """
 
 layout = [
-    [sg.Text("Youtube URL:"), sg.Input(key="urlinput")],
-    [sg.Text("Output folder:"), sg.Input(key="vidoutput")],
+    [sg.Text("Youtube URL:"), sg.InputText()],
+    [sg.Text("Output folder:"), sg.Input(key="vidoutput"), sg.FolderBrowse()],
     [sg.Exit(), sg.Button(button_text="Download Video",key="download")],
     ]
     
-window = sg.Window("Demo", layout=layout,).read()
+window = sg.Window("Demo", layout=layout)
 
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event in (sg.WINDOW_CLOSED, "Exit"):
-        print("User initiated exit, breaking..")
-        break
-    if event == "download":
-        sg.popup_error("Adding this")
+def runwindow():
+    while True:
+        global event, values
+        event, values = window.read()
+        if event in (sg.WINDOW_CLOSED, "Exit"):
+            print("User initiated exit, breaking..")
+            break
+        if event == "download":
+            videodownload()
 
-window.close()
+    window.close()
+
+def videodownload():
+    """    ytdl_format_options = {
+        'outmpl': values[1]
+        } """
+    with youtube_dl.YoutubeDL() as ydl:
+                ydl.download([values[0]])
+
+def main():
+    runwindow()
+
+if __name__ == "__main__":
+    main()
