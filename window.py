3f8ac985fe29b17982c2ccb218b35d9d9cad100c
tjensen <49765787+tjensen43@users.noreply.github.com>
2022-10-12 12:49:26 -0400

    Added text input fields & main while loop

--- window.py
+++ window.py
@@ -1,9 +1,26 @@
 from __future__ import unicode_literals
+from cmath import log
 import youtube_dl
 
 import PySimpleGUI as sg
 
 #rest of the code
 
-layout = [[sg.Text("Test Text")]]
-sg.Window("Demo", layout=layout, margins=(100, 50)).read()
+layout = [
+    [sg.Text("Youtube URL:"), sg.Input(key="urlinput")],
+    [sg.Text("Output folder:"), sg.Input(key="vidoutput")],
+    [sg.Exit(), sg.Button(button_text="Download Video",key="download")],
+    ]
+    
+window = sg.Window("Demo", layout=layout,).read()
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event in (sg.WINDOW_CLOSED, "Exit"):
+        print("User initiated exit, breaking..")
+        break
+    if event == "download":
+        sg.popup_error("Adding this")
+
+window.close()
